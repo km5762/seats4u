14bb250313a5f0be70feab155d31d6da9bcb2a7b
echandler5956f <ethanchandler5956f@gmail.com>
2023-11-28 16:40:35 -0500

    Got rid of cringe Api

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -13,18 +13,3 @@ export function createShowC(manager, name, date, time){
     manager.venue.addShow(name, date, time);
     console.log(manager);
 }
-
-export function listVenuesC(admin){
-    // potentially modify the model
-    let nameField = document.getElementById("venues")
-
-     // prepare payload for the post
-    let data = { 'name': nameField.value }
- 
-    const handler = (json) => {
-        console.log(json)
-        // clear inputs
-        nameField.value = ''
-    }
-
-    post('/listvenues', data, handler)}
